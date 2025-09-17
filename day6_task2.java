e0c7b075b3941762931fec5c98bec2f98bd49dfa
AJAL <164057103+AJALJAMES@users.noreply.github.com>
2025-09-17 22:52:19 +0530

    Add files via upload

---
+++ day6_task2.java
@@ -0,0 +1,30 @@
+public class day6_task2 {
+    public static void main(String[]args){
+        double average=0;
+        int total=0;
+        int[]marks= {78,82,91,65,34};
+
+        for(int i=0;i<marks.length;i++){
+             total+=marks[i];
+               average=total/marks.length;
+               if(marks[i]<=35){
+                System.out.println("Fail");
+               }
+    }
+             System.out.println("The total marks is:" +total);
+             System.out.println("The average is:"+average);
+
+             if(average>=90){
+                System.out.println("Grade:A");
+             }
+             else if(average>=75 && average<=89){
+                System.out. println("Grade:B");
+             }
+              else if(average>=60 && average<=74){
+                System.out.println("Grade:C");
+              }
+              else if(average<60){
+                System.out.println("Grade:D");}
+        }
+    }
+
